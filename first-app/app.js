cfa92750931279058e9c5ac06762f80c0794d6fd
Geoffrey House <geo.house7@gmail.com>
2022-11-12 19:04:40 -0700

    node server updates

--- first-app/app.js
+++ first-app/app.js
@@ -1,9 +1,58 @@
 // run as node app.js from powershell
 
+// built-in modules
+// path is object
+const path = require("path");
+// Node calls all modules after wrapping them in an immediately invoked function.
+// One of the parameters to that function is the filename, which is
+// accessible using the __filename variable.
+// Gives object with root, dir, base, ext and name keys.
+const pathObj = path.parse(__filename);
+console.log(pathObj);
+
+const os = require("os");
+let totalMemory = os.totalmem();
+let freeMemory = os.freemem();
+let lineEnding = os.EOL;
+
+const fs = require("fs");
+const filesSync = fs.readdirSync("./");
+console.log(filesSync);
+
+// loading 'events' returns the EventEmitter Class
+const EventEmitter = require("events");
+//const emitter = new EventEmitter();
+
+// Async versions of the fs methods take a callback function as their last
+// argument. Node calls this function when the async operation completes.
+const files = fs.readdir("./", function (err, files) {
+  // only 1 of err, files will be filled; the other will be null
+  if (err) console.log("Error", err);
+  else console.log("Result", files);
+});
+
+console.log(`Total memory is: ${totalMemory}`);
+console.log(`Free memory is: ${freeMemory}`);
+console.log(`Line ending is: ${lineEnding}`);
+
 // load module
 // could also load just a 'logger' and Node looks for .js extension
-let logger = require("./logger.js");
-console.log(logger);
+const Logger = require("./logger.js");
+//console.log(logger);
+const logger = new Logger();
+
+// register a listener (same as .addListener())
+// arg param is the object argument given in the emitter.emit() call below
+logger.on("messageLogged", (arg) => {
+  console.log("Listener called");
+  console.log(arg);
+});
+
+// Raise: logging (data: message)
+
+logger.on("logging", (arg) => {
+  console.log(arg);
+});
 
 logger.log("message");
 
@@ -29,4 +78,26 @@ sayHello("Gus");
 // with self-contained scope (unless things are explicitly exported)
 
 // module is a JSON object
-console.log(module);
+//console.log(module);
+
+const http = require("http");
+// server is an event emitter. so can run emit(), on(), methods, etc.
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+
+  if ((req.url = "/api/courses")) {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+
+// server.on("connection", (socket) => {
+//   console.log("New connection...");
+// });
+
+server.listen(3000);
+
+console.log("listening on port 3000...");
--- first-app/logger.js
+++ first-app/logger.js
@@ -1,14 +1,26 @@
+const EventEmitter = require("events");
+//const emitter = new EventEmitter();
+
 // made up
 const url = "http://mylogger.io/log";
 
-function log(message) {
-  // Send HTTP request
-  console.log(message);
+class Logger extends EventEmitter {
+  log(message) {
+    // Send HTTP request
+    console.log(message);
+
+    // use to raise an event; need to register a listener that
+    // listens for when the messageLogged event is raised.
+    this.emit("messageLogged", { id: 1, url: "http://" });
+
+    this.emit("logging", { data: "Here's the message!" });
+  }
 }
 
 // Exporting for use in app.js module
 
-module.exports.log = log;
+//module.exports.Logger = Logger;
+module.exports = Logger;
 // don't need to export (used in this module only)
 //module.exports.url = url;
 
